d663792927585978749e7f3769efa7d6ac06f029
DamianoLandi <damianolandi01@gmail.com>
2021-07-22 16:49:27 +0200

    Creazione Carta

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var card = {
+    nome: "Soldato dell'Oscuro Desiderio",
+    costo: ["4", "R", "U"],
+    costoConvertito: 6,
+    tipo: {
+        primoTipo: "Creatura",
+        archetipo: "Soldato",
+    },
+    espansione: 7,
+    effetti: [
+        {
+            costo: ["B", "R"],
+            effetto: "Infliggi 700 danni al nemico",
+        },
+        {
+            costo: ["U", "T"],
+            effetto: "Riduci il costo della prossima abilità utilizzata di 1 mana",
+        }
+    ],
+    testoColore: "Un antico soldato che, dopo secoli, continua a seguire gli ordini del suo padrone...",
+    illustratore: "John Smith",
+    idCarta: {
+        numero: 7,
+        totale: 420,
+    },
+    forza: 13,
+    costituzione: 11,
+    bordo: "Dorato",
+}
